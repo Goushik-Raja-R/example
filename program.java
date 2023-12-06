233ad4c42e4428949943724dccdf2997d538313e
Goushik-Raja-R <goushikraja14@gmail.com>
2023-12-06 21:17:55 +0530

    modifies the file

--- program.java
+++ program.java
@@ -1,5 +1,5 @@
 
-public class smallletter {
+public class program {
     
     public static void main(String[] args) {
         
@@ -7,5 +7,23 @@ public class smallletter {
         char target='a';
         char ans=smalllettersearch(letters,target);
         System.out.println("The next greatest character is: "+ans);
+    }
+
+     public static char smalllettersearch(char []letters,char target)
+    {
+        int start=0;
+        int end=letters.length-1;
+
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+
+            if(target<letters[mid])
+            end=mid-1;
+            
+            else
+            start=mid+1;
+        }
+        return letters[start % letters.length];
     }
 }
