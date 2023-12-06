21380075de96c952fe5836ef7d8038e837c61f2b
Goushik raja <117624643+Goushik-Raja-R@users.noreply.github.com>
2023-12-06 14:30:56 +0530

    Create program.java

---
+++ program.java
@@ -0,0 +1,11 @@
+
+public class smallletter {
+    
+    public static void main(String[] args) {
+        
+        char letters[]={'c','f','j'};
+        char target='a';
+        char ans=smalllettersearch(letters,target);
+        System.out.println("The next greatest character is: "+ans);
+    }
+}
